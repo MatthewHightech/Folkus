a093640f1e71f89835a0a50cabd1acf3b9f8c741
MatthewHightech <Smoose007@gmail.com>
2021-12-11 20:10:18 -0800

    First Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss', 'wave.scss'],
 })
 export class HomePage {
 
   constructor() {}
+
+  raiseBar() {
+    const waves:any = document.getElementsByClassName("waveWrapperInner");
+
+    for(let i = 0; i < waves.length; i++) {
+      let currentBottom = window.getComputedStyle(waves[i]).getPropertyValue("bottom").split("px")
+      console.log(currentBottom)
+      waves[i].style.bottom = (parseInt(currentBottom[0]) + 100) + "px";
+    }
+  }
+
+  startTimer(minute, sec) {
+    setInterval(function() {
+      document.getElementById("timer").innerHTML = minute + " : " + sec;
+      sec--;
+      if (sec == 0) {
+        minute --;
+        sec = 60;
+        if (minute == 0) {
+          minute = 5;
+        }
+      }
+    }, 1000);
+  }
 
 }
